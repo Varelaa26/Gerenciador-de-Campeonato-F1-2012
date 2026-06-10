eaa022bf03a159bb75d3280db8480b406a096a4b
Victor Gabriel <victorgdsv2011@gmail.com>
2026-06-10 20:05:02 -0300

    0.2.2
    Adicionar pontos incluídos em add_win()

--- driver.py
+++ driver.py
@@ -1,11 +1,16 @@
+from Classes.team import Team
+
 class Driver:
-    def __init__(self, num, name, team, points = 0, position = 0, wins = 0):
+    def __init__(self, num: int, name: str, team_object: Team, points: int = 0, position: int = 0, wins: int = 0):
         self.num = num
         self.name = name
-        self.team = team
+        self.team = team_object
         self.points = points
         self.position = position
         self.wins = wins
 
     def add_win(self):
-        self.wins +=1
+        self.wins += 1
+        self.points += 25
+        self.team.wins += 1
+        self.team.wins += 25
